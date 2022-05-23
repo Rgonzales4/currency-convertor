d63f18d04cf74980e7168ca9cb078b00f1615a64
Rgonzales4 <roshel.gonzales@yahoo.com>
2022-05-23 16:40:00 +1000

    feat: Starting currency convertor mini project

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# REACT Currency Convertor project
+
---
+++ src/App.js
@@ -0,0 +1,8 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (<h1> Currency Convertor </h1>);
+}
+
+export default App;
